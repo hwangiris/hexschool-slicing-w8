e26bff72ba9a61248935d5d3f92a132636d2bb57
hwangiris <42462259+hwangiris@users.noreply.github.com>
2022-09-13 00:16:24 +0800

    work, series, rank, artist

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,38 @@
 $(function() {
-  console.log('Hello Bootstrap5');
+  var updateMasonry = function() {
+    $('.cards--artwork').masonry({
+      itemSelector: '.cards--artwork > div',
+    });
+  }
+  var updateWork = function() {
+    console.log('a');
+    $('.cards--info').masonry({
+      itemSelector: '.cards--info > div',
+    });
+  }
+  var masonry = function() {
+    updateMasonry();
+    updateWork();
+  }
+  $('button[data-bs-toggle="tab"]').on('shown.bs.tab', masonry);
+  $('button.toggle-arrow + .collapse').on('shown.bs.collapse', masonry);
+  $('button.toggle-arrow + .collapse').on('hidden.bs.collapse', masonry);
+  $(window).on('resize load', masonry);
+  
+  var swiper = new Swiper('.swiper--artist', {
+    slidesPerView: 1,
+    centeredSlides: true,
+    loop: true,
+    spaceBetween: 12,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+      },
+    },
+  });
 });
